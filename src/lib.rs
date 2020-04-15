10a079d08d6801954e907d968bbc70203c12f4aa
Xymist <james@jamieduerden.me>
2020-04-15 20:56:32 +0100

    Add Solver, with cover and uncover operations

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ type Index = usize;
 #[derive(Clone, Debug, Default, PartialEq)]
 // Represents a 1 in the sparse matrix, or a header entry. Contains links to nearest
 // neighbours plus some header data if appropriate.
-pub struct MtxElem {
+struct MtxElem {
     // The index of the column header, or itself if this is a header
     col: Index,
     // Row index (0 for headers)
@@ -22,43 +22,23 @@ pub struct MtxElem {
     down: Index,
 }
 
-#[derive(Clone, Debug, Default)]
-pub struct Mtx {
+/// An assembled matrix, for solving a specific problem
+struct Solver {
     pub subset_rows: HashMap<usize, usize>,
     pub elements: Vec<MtxElem>,
     pub bottoms: Vec<Index>,
+
+    // The number of columns still participating in the algorithm. If this
+    // falls to zero, a solution has been found.
     pub active_columns: usize,
 }
 
-impl Mtx {
-    pub fn last_row(&self) -> Index {
-        match self.elements.get(self.elements.len() - 1) {
-            Some(i) => i.row,
-            None => 0,
-        }
-    }
-
-    pub fn size(&self) -> usize {
-        self.elements.len()
-    }
-
-    pub fn with_capacity(capacity: usize) -> Self {
-        Mtx {
-            subset_rows: HashMap::new(),
-            elements: Vec::with_capacity(capacity),
-            bottoms: Vec::with_capacity(capacity),
-            active_columns: 0,
-        }
-    }
-
-    pub fn connect_vertical(&mut self) {
-        for (i, &b) in self.bottoms.iter().enumerate() {
-            self.elements[b].down = i;
-            self.elements[i].up = b;
-        }
-    }
-
-    pub fn smallest_column(&self) -> Index {
+impl Solver {
+    // For reasons of efficiency, we choose the column containing
+    // the smallest number of ones (which in this sparse matrix is
+    // the smallest column) at each iteration of the algorithm.
+    // This minimises the number of levels in each branch of the tree.
+    fn smallest_column(&self) -> Index {
         let mut smol_count = Index::max_value();
         let mut smol_idx: Index = 0;
         let mut col_idx: Index = 0;
@@ -75,6 +55,220 @@ impl Mtx {
         }
 
         smol_idx
+    }
+
+    pub fn solve(&mut self) {}
+
+    fn subsolve(&mut self) {}
+
+    fn cover(&mut self, col_idx: Index) {
+        // Find the horizontal neighbours of the chosen column
+        let r = self.elements[col_idx].right;
+        let l = self.elements[col_idx].left;
+
+        // Remove this header by attaching its right to its left
+        // and it's left to its right.
+        self.elements[r].left = self.elements[col_idx].left;
+        self.elements[l].right = self.elements[col_idx].right;
+
+        // Take the next element of this column, 'downwards'
+        let mut nxt = self.elements[col_idx].down;
+
+        // Do the same thing by joining the top and bottom for every other one
+        // of every row which has a 1 in that column.
+        while nxt != col_idx {
+            let mut rgt = self.elements[nxt].right;
+            while rgt != nxt {
+                // Find the header for this column and reduce its count
+                // of ones by one
+                let header = self.elements[rgt].col;
+                self.elements[header].size -= 1;
+
+                let col_up = self.elements[rgt].up;
+                self.elements[col_up].down = self.elements[rgt].down;
+                let col_down = self.elements[rgt].down;
+                self.elements[col_down].up = self.elements[rgt].up;
+
+                rgt = self.elements[rgt].right;
+            }
+            nxt = self.elements[nxt].down;
+        }
+    }
+
+    fn uncover(&mut self, col_idx: Index) {
+        // Iterate backwards, restoring the rows previously removed.
+        // Starts with the 'top' of the header and works upwards,
+        // as the cover operation starts with the header and works down.
+        let mut nxt = self.elements[col_idx].up;
+
+        while nxt != col_idx {
+            // As with the vertical traversal, this is inverted; it
+            // travels 'left' around the loop rather than 'right'
+            let mut lft = self.elements[nxt].left;
+            while lft != nxt {
+                // Increase the ones-count of the header by one
+                let col_hdr = self.elements[lft].col;
+                self.elements[col_hdr].size += 1;
+
+                let col_up = self.elements[lft].up;
+                self.elements[col_up].down = lft;
+
+                let col_down = self.elements[lft].down;
+                self.elements[col_down].up = lft;
+
+                lft = self.elements[lft].left;
+            }
+
+            nxt = self.elements[nxt].up;
+        }
+
+        // Restore the column header
+        let r = self.elements[col_idx].right;
+        self.elements[r].left = col_idx;
+
+        let l = self.elements[col_idx].left;
+        self.elements[l].right = col_idx;
+    }
+}
+
+impl From<Mtx> for Solver {
+    fn from(m: Mtx) -> Self {
+        Solver {
+            subset_rows: m.subset_rows,
+            elements: m.elements,
+            active_columns: m.bottoms.len(),
+            bottoms: m.bottoms,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Mtx {
+    // Matches each row in the matrix with the ID of its subset,
+    // for later analysis
+    pub subset_rows: HashMap<usize, usize>,
+    // The matrix itself
+    pub elements: Vec<MtxElem>,
+    // The 'bottom' elements of each column; i.e. the populated element
+    // in that column with the highest row index
+    pub bottoms: Vec<Index>,
+}
+
+impl Mtx {
+    pub fn size(&self) -> usize {
+        self.elements.len()
+    }
+
+    pub fn last_row(&self) -> Index {
+        match self.elements.get(self.elements.len() - 1) {
+            Some(i) => i.row,
+            None => 0,
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        let matrix = Mtx {
+            subset_rows: HashMap::new(),
+            elements: Vec::with_capacity(capacity),
+            bottoms: Vec::with_capacity(capacity),
+        };
+
+        matrix.push_headers(capacity)
+    }
+
+    fn push_headers(mut self, column_count: usize) -> Self {
+        // Push the first header element
+        self.elements.push(MtxElem {
+            col: 0,
+            right: 1,
+            left: column_count - 1,
+            ..Default::default()
+        });
+
+        // Push the middle N headers, linked left and right
+        for idx in 1..(column_count - 1) {
+            self.elements.push(MtxElem {
+                col: idx,
+                right: idx + 1,
+                left: idx - 1,
+                ..Default::default()
+            })
+        }
+
+        // Push the final header element and wrap
+        self.elements.push(MtxElem {
+            col: column_count - 1,
+            right: 0,
+            left: column_count - 2,
+            ..Default::default()
+        });
+
+        self.bottoms.extend(0..column_count);
+
+        self
+    }
+
+    pub fn push_subsets<T: PartialEq>(
+        mut self,
+        collection: &[Subset<T>],
+        universe: &Universe<T>,
+    ) -> Self {
+        for subset in collection.iter() {
+            self.push_subset_as_row(subset, universe)
+        }
+        self
+    }
+
+    fn push_subset_as_row<T: PartialEq>(&mut self, s: &Subset<T>, u: &Universe<T>) {
+        let row = self.last_row() + 1;
+        let row0 = self.size();
+
+        // Empty subsets can be ignored, since they don't contribute
+        // to the union.
+        if s.elements.len() == 0 {
+            return;
+        };
+
+        for (col, c) in u.iter().enumerate() {
+            if s.elements.contains(&c) {
+                let idx = self.size();
+                let elem = MtxElem {
+                    col,
+                    row,
+                    left: idx - 1,
+                    right: idx + 1,
+                    up: self.bottoms[col],
+                    ..Default::default()
+                };
+
+                self.elements.push(elem);
+
+                self.elements[self.bottoms[col]].down = idx;
+
+                self.bottoms[col] = idx;
+
+                self.elements[col].size += 1;
+            }
+        }
+        let rown = self.elements.len() - 1;
+
+        self.elements[row0].left = rown;
+        self.elements[rown].right = row0;
+
+        // This allows us to determine from the solution the exact subsets
+        // which comprise it, even when they contain the same elements.
+        self.subset_rows.entry(row).or_insert(s.id);
+    }
+
+    // Connects the 'bottom' of each column to the 'top',
+    // turning the matrix from a ring into a torus. This
+    // precludes adding more rows
+    pub fn finalise(mut self) -> Solver {
+        for (i, &b) in self.bottoms.iter().enumerate() {
+            self.elements[b].down = i;
+            self.elements[i].up = b;
+        }
+        self.into()
     }
 }
 
@@ -100,95 +294,15 @@ impl<T: PartialEq> Subset<T> {
             elements,
         }
     }
-
-    pub fn push_as_row(&self, matrix: &mut Mtx, u: &Universe<T>) {
-        let row = matrix.last_row() + 1;
-        let row0 = matrix.size();
-
-        // Empty subsets can be ignored, since they don't contribute
-        // to the union.
-        if self.elements.len() == 0 {
-            return;
-        };
-
-        for (col, c) in u.iter().enumerate() {
-            if self.elements.contains(&c) {
-                let idx = matrix.size();
-                let elem = MtxElem {
-                    col,
-                    row,
-                    left: idx - 1,
-                    right: idx + 1,
-                    up: matrix.bottoms[col],
-                    ..Default::default()
-                };
-
-                matrix.elements.push(elem);
-
-                matrix.elements[matrix.bottoms[col]].down = idx;
-
-                matrix.bottoms[col] = idx;
-
-                matrix.elements[col].size += 1;
-            }
-        }
-        let rown = matrix.elements.len() - 1;
-
-        matrix.elements[row0].left = rown;
-        matrix.elements[rown].right = row0;
-
-        // This allows us to determine from the solution the exact subsets
-        // which comprise it, even when they contain the same elements.
-        matrix.subset_rows.entry(row).or_insert(self.id);
-    }
 }
 
 pub fn weighted_exact_cover<T: PartialEq>(
     u: Universe<T>,
     c: Collection<T>,
 ) -> Option<Vec<Subset<T>>> {
-    let mut solution = None;
+    let _solver = Mtx::with_capacity(u.len()).push_subsets(&c, &u).finalise();
 
-    let mut matrix = Mtx::with_capacity(u.len());
-    let column_count = u.len();
-    push_headers(&mut matrix, column_count);
-    for subset in c.iter() {
-        subset.push_as_row(&mut matrix, &u)
-    }
-    matrix.connect_vertical();
-
-    return solution;
-}
-
-fn push_headers(matrix: &mut Mtx, column_count: usize) {
-    // Push the first header element
-    matrix.elements.push(MtxElem {
-        col: 0,
-        right: 1,
-        left: column_count - 1,
-        ..Default::default()
-    });
-
-    // Push the middle N headers, linked left and right
-    for idx in 1..(column_count - 1) {
-        matrix.elements.push(MtxElem {
-            col: idx,
-            right: idx + 1,
-            left: idx - 1,
-            ..Default::default()
-        })
-    }
-
-    // Push the final header element and wrap
-    matrix.elements.push(MtxElem {
-        col: column_count - 1,
-        right: 0,
-        left: column_count - 2,
-        ..Default::default()
-    });
-
-    matrix.bottoms.extend(0..column_count);
-    matrix.active_columns = column_count;
+    return None;
 }
 
 #[cfg(test)]
